5fb3ed4da63adf66b9b9e81810c837d55cb376c9
Emilio Reyes <emilio.reyes@intel.com>
2022-11-04 20:36:46 -0700

    initial code commit
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,122 @@
-# sok
+[![vulnerabilities](https://img.shields.io/badge/vulnerabilities-None-brightgreen)](https://pypi.org/project/bandit/)
+[![python](https://img.shields.io/badge/python-3.7%20%7C%203.8%20%7C%203.9%20%7C%203.10-teal)](https://www.python.org/downloads/)
+# wait-for-message
+
+A simple client server utility that blocks until a message is received on a TCP/IP socket connection; useful for synchronizing interdependent networked jobs.
+
+## `w4m` Usage
+```bash
+usage: w4m [-h] {send,wait} ...
+
+A simple client server utility that blocks until a message is received on a TCP/IP socket connection
+
+positional arguments:
+  {send,wait}
+    send       send message to server
+    wait       wait for message from client
+
+optional arguments:
+  -h, --help   show this help message and exit
+```
+
+### `w4m send`
+```bash
+usage: w4m send [-h] --ip-address IP_ADDRESS --port-number PORT_NUMBER --message MESSAGE_TO_SEND [--delay DELAY] [--attempts MAX_ATTEMPTS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ip-address IP_ADDRESS
+                        the ip address of the server
+  --port-number PORT_NUMBER
+                        the port number the server is listening on
+  --message MESSAGE_TO_SEND
+                        the message to send
+  --delay DELAY         number of seconds to delay between retries; default 10
+  --attempts MAX_ATTEMPTS
+                        maximum retry attempts; default 6
+```
+
+### `w4m wait`
+```bash
+usage: w4m wait [-h] --port-number PORT_NUMBER --message MESSAGE_TO_WAIT_FOR [--timeout TIMEOUT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --port-number PORT_NUMBER
+                        the port number the server is listening on
+  --message MESSAGE_TO_WAIT_FOR
+                        the message to wait for
+  --timeout TIMEOUT     number of seconds to wait for message; default 900 (i.e. 15 minutes)
+```
+
+## Execution
+
+client
+```
+w4m send --ip-address 192.168.1.199 --port-number 8080 --message 'ready to proceed'
+
+2022-11-05 16:56:42,724: creating tcp/ip socket
+2022-11-05 16:56:42,724: connecting to server 192.168.1.199:8080
+2022-11-05 16:56:42,725: sending message 'ready'
+2022-11-05 16:56:42,725: received acknowledgement: 'acknowledged - waiting'
+2022-11-05 16:56:42,725: closing tcp/ip socket
+
+w4m send --ip-address 192.168.1.199 --port-number 8080 --message 'ready2'
+2022-11-05 16:56:54,014: creating tcp/ip socket
+2022-11-05 16:56:54,014: connecting to server 192.168.1.199:8080
+2022-11-05 16:56:54,014: sending message 'ready2'
+2022-11-05 16:56:54,015: received acknowledgement: 'acknowledged - waiting'
+2022-11-05 16:56:54,015: closing tcp/ip socket
+
+w4m send --ip-address 192.168.1.199 --port-number 8080 --message 'ready to proceed'
+2022-11-05 16:56:58,520: creating tcp/ip socket
+2022-11-05 16:56:58,520: connecting to server 192.168.1.199:8080
+2022-11-05 16:56:58,520: sending message 'ready to proceed'
+2022-11-05 16:56:58,521: received acknowledgement: 'acknowledged - quitting'
+2022-11-05 16:56:58,521: closing tcp/ip socket
+```
+
+server
+```
+w4m wait --port-number 8080 --message 'ready to proceed'
+
+2022-11-05 16:56:33,717: creating tcp/ip socket
+2022-11-05 16:56:33,717: binding and starting up on 652b723eb830:8080
+2022-11-05 16:56:33,717: waiting for message
+2022-11-05 16:56:42,725: accepted connection from ('172.17.0.1', 42148)
+2022-11-05 16:56:42,725: message received: 'ready'
+2022-11-05 16:56:42,725: sending acknowledgement message: 'acknowledged - waiting'
+2022-11-05 16:56:42,725: shutting down and closing connection
+2022-11-05 16:56:42,725: waiting for message
+2022-11-05 16:56:54,015: accepted connection from ('172.17.0.1', 42152)
+2022-11-05 16:56:54,015: message received: 'ready2'
+2022-11-05 16:56:54,015: sending acknowledgement message: 'acknowledged - waiting'
+2022-11-05 16:56:54,015: shutting down and closing connection
+2022-11-05 16:56:54,015: waiting for message
+2022-11-05 16:56:58,520: accepted connection from ('172.17.0.1', 42156)
+2022-11-05 16:56:58,520: message received: 'ready to proceed'
+2022-11-05 16:56:58,520: the message being waited for was received
+2022-11-05 16:56:58,520: sending acknowledgement message: 'acknowledged - quitting'
+2022-11-05 16:56:58,520: shutting down and closing connection
+2022-11-05 16:56:58,520: closing tcp/ip socket
+```
+
+## Development
+
+Build the Docker image:
+```
+docker image build \
+-t \
+w4m:latest .
+```
+
+Run the Docker container:
+```
+docker container run \
+--rm \
+-it \
+-v $PWD:/code \
+-p:8080:8080 \
+w4m:latest \
+bash
+```
---
+++ src/main/python/wait_for_message/cli.py
@@ -0,0 +1,197 @@
+import sys
+import time
+import socket
+import logging
+import argparse
+
+logger = logging.getLogger(__name__)
+
+
+class MaxAttemptsError(Exception):
+    """ maximum number of attempts
+    """
+    pass
+
+
+def get_parser():
+    """ return argument parser
+    """
+    parser = argparse.ArgumentParser(
+        description='A simple client server utility that blocks until a message is received on a TCP/IP socket connection')
+    subparser = parser.add_subparsers(dest='command')
+
+    send_parser = subparser.add_parser(
+        'send',
+        help='send message to server')
+    send_parser.set_defaults(subcmd=send)
+    send_parser.add_argument(
+        '--ip-address',
+        dest='ip_address',
+        type=str,
+        required=True,
+        help='the ip address of the server')
+    send_parser.add_argument(
+        '--port-number',
+        dest='port_number',
+        type=int,
+        required=True,
+        help='the port number the server is listening on')
+    send_parser.add_argument(
+        '--message',
+        dest='message_to_send',
+        type=str,
+        required=True,
+        help='the message to send')
+    send_parser.add_argument(
+        '--delay',
+        dest='delay',
+        type=int,
+        default=10,
+        help='number of seconds to delay between retries; default 10')
+    send_parser.add_argument(
+        '--attempts',
+        dest='max_attempts',
+        type=int,
+        default=6,
+        help='maximum retry attempts; default 6')
+
+    wait_parser = subparser.add_parser(
+        'wait',
+        help="wait for message from client")
+    wait_parser.set_defaults(subcmd=wait)
+    wait_parser.add_argument(
+        '--port-number',
+        dest='port_number',
+        type=int,
+        required=True,
+        help='the port number the server is listening on')
+    wait_parser.add_argument(
+        '--message',
+        dest='message_to_wait_for',
+        type=str,
+        required=True,
+        help='the message to wait for')
+    wait_parser.add_argument(
+        '--timeout',
+        dest='timeout',
+        type=int,
+        default=60 * 15,
+        help='number of seconds to wait for message; default 900 (i.e. 15 minutes)')
+
+    return parser
+
+
+def send(ip_address, port_number, message_to_send, delay, max_attempts):
+    """ send message to server at ip_address:port_number
+    """
+    logger.debug('creating tcp/ip socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+
+    attempts = 0
+    while True:
+        if attempts >= max_attempts:
+            error_message = f'exceeded maximum wait of {max_attempts * delay} seconds'
+            logger.debug(error_message)
+            raise MaxAttemptsError(error_message)
+        try:
+            logger.debug(f'connecting to server {ip_address}:{port_number}')
+            s.connect((ip_address, port_number))
+
+            logger.debug(f"sending message '{message_to_send}'")
+            s.send(message_to_send.encode())
+
+            # acknowledge the message was received
+            # if no acknowledgement then connect and resend
+            acknowledgement = s.recv(10000).decode()
+            logger.debug(f"received acknowledgement: '{acknowledgement}'")
+            break
+
+        except socket.error as error:
+            logger.debug(f'socket.error: {error}')
+            logger.debug(f'retrying in {delay} seconds')
+            time.sleep(delay)
+            attempts += 1
+
+    logger.debug('closing tcp/ip socket')
+    s.close()
+
+
+def wait(port_number, message_to_wait_for, timeout):
+    """ setup socket on port_number and wait for message
+    """
+    logger.debug('creating tcp/ip socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # PREVENT FROM "ADDRESS ALREADY IN USE" UPON restart
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.settimeout(timeout)
+
+    server_name = socket.gethostname().lower()
+    logger.debug(f'binding and starting up on {server_name}:{port_number}')
+    server_address = (server_name, port_number)
+    s.bind(server_address)
+
+    s.listen(5)
+
+    while True:
+        logger.debug('waiting for message')
+        connection, client_address = s.accept()
+        logger.debug(f'accepted connection from {connection.getpeername()}')
+
+        data = connection.recv(10000).decode()
+        logger.debug(f"message received: '{data}'")
+
+        message_received = False
+        if data == message_to_wait_for:
+            logger.debug('the message being waited for was received')
+            message_received = True
+
+        status = 'quitting' if message_received else 'waiting'
+        acknowledgement = f'acknowledged - {status}'
+        logger.debug(f"sending acknowledgement message: '{acknowledgement}'")
+        connection.send(acknowledgement.encode())
+
+        logger.debug('shutting down and closing connection')
+        connection.shutdown(socket.SHUT_RD | socket.SHUT_WR)
+        connection.close()
+
+        if message_received:
+            break
+
+    logger.debug('closing tcp/ip socket')
+    s.close()
+
+
+def run(options):
+    """ run subcmd
+    """
+    options.pop('command', None)
+    subcmd = options.pop('subcmd', None)
+    subcmd(**options)
+
+
+def configure_logging():
+    """ configure logging
+    """
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s: %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
+def main():
+    """ main program
+    """
+    # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    configure_logging()
+    args = get_parser().parse_args()
+    run(vars(args))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
